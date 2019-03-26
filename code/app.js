08c4059cbcf9d1563a219a4fd7792a947c27bffa
daniele leo <danieleleo16@gmail.com>
2019-03-26 09:36:45 +0100

    Trailer ENDGAME

--- code/app.js
+++ code/app.js
@@ -1,4 +1,23 @@
-var init = function(){
+var accedi=function(){
+    console.log("Hai cliccato su accedi");
+    var username = $("#username>input").val();
+    console.log("username: "+ username);
+    var password = $("#password>input").val();
+    console.log("password: "+ password);
+    var auth= btoa(username+password);
+    console.log("auth: "+auth);
+
+    $.get("http://10.3.0.205:3000/login?auth="+auth,
+    accediRisposta);
+
+}
+
+var accediRisposta = function(data){
+    console.log(data);
+    alert(data);
+}
+
+var init  = function(){
     console.log("Ciao, sono Jonny Sins.");
     $("title").text("Marconi Alternanza prova");
 
@@ -6,6 +25,8 @@ var init = function(){
     .attr("placeholder","Jonny");
     $("#password>input")
     .attr("placeholder","Password");
+
+    $("#accedi>button").on("click",accedi);
 }
 
 
